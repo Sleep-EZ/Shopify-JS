47497f342d7ebea6bcf9ed34fc75ec2835b8a64f
Evan Darwin <evan@sleepez.com>
2018-01-05 18:58:03 -0700

    Files were accidentally excluded

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import Client from './client';
+import Cache from './cache';
+import * as Storage from './storage';
+import * as Types from './types';
+import * as Lib from './lib';
+
+export {
+    Client,
+    Cache,
+    Storage,
+    Types,
+    Lib
+};
