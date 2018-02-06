d52ab66ccf3aa5872fae9e02cf4ea8687a48888d
Evan Darwin <evan@sleepez.com>
2018-02-06 16:05:01 -0700

    Export all items in types.ts by default

--- dist/build/index.d.ts
+++ dist/build/index.d.ts
@@ -1,9 +1,3 @@
-import { Client } from './client';
-import { ForageStorageDriver } from './storage';
-declare const _default: {
-    Client: typeof Client;
-    Storage: {
-        ForageStorageDriver: typeof ForageStorageDriver;
-    };
-};
-export = _default;
+export * from './types';
+export { Client } from './client';
+export { ForageStorageDriver } from './storage';
